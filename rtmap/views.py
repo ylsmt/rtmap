56c37797e5c8facca00d0d9456872db222214bfb
ylsmt <yl5tsmt@163.com>
2017-10-24 12:08:14 +0800

    添加表格显示

--- rtmap/views.py
+++ rtmap/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rtmap.models import Router
 
 # Create your views here.
+
+def index(request):
+    router_list = Router.objects.all().order_by('ip')
+    context = {
+            'router_list': router_list,
+            }
+
+    return render(request,'rtmap/list.html',context)
+
+
+def map_view(request):
+
